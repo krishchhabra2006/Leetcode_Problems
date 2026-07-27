806748b0f8faede4461ab3ee1fa2a3adb509bc7a
Krish chhabra <ckrish.9084@gmail.com>
2026-07-27 13:43:50 +0530

    Time: 0 ms (100%), Space: 42.3 MB (23.24%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int ans=x^y;
+        int count=0;
+        for(int i=0;i<31;i++){
+            if((ans & (1<<i))!=0) count++;
+
+        }
+        return count;
+    }
+}
