f3012e9f136fceadf8848cf8b868cd14b792e9e3
Krish chhabra <ckrish.9084@gmail.com>
2026-09-13 15:08:58 +0530

    Time: 0 ms (100%), Space: 50.5 MB (11.28%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hIndex(int[] cit) {
+        int n=cit.length;
+        int l=0,r=n-1,ans=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(cit[mid]>=n-mid){
+                ans=n-mid;
+                r=mid-1;
+
+            }
+            else l=mid+1;
+        }
+        return ans;
+        
+    }
+}
